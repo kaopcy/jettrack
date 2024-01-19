1ea2fd09cd106a8844a9c70fb0fa2c57bf1c234b
piyachai kaewchum <kaonaka@hotmail.co.th>
2024-01-19 14:43:18 +0700

    setup/add-custom-font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,27 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+import { Patrick_Hand } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const nuraFont = localFont({
+  src: [
+    {
+      path: "../../public/fonts/nura/nura.ttf",
+    },
+  ],
+  variable: "--font-nura",
+});
+
+const virgilFont = localFont({
+  src: [
+    {
+      path: "../../public/fonts/virgil/virgil.woff2",
+    },
+  ],
+  variable: "--font-virgil",
+});
+
+const patrickFont = Patrick_Hand({ weight: ["400"], subsets: ["latin"], variable: "--font-patrickhand" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +31,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${nuraFont.variable} ${virgilFont.variable} ${patrickFont.variable}`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { env } from "@/configs/env";
 export default function Home() {
   return (
     <main>
-      <div className="">{env.googleAuthSecret}</div>
+      <div className="font-patrick ">{env.googleAuthSecret}</div>
+      <div className="font-nura text-xl">ว่าไงไอหน้าฮี</div>
     </main>
   );
 }
