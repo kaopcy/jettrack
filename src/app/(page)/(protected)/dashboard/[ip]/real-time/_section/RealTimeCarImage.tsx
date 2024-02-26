83470b342a09223137779ff2ad93c4f63d4b87a0
pkaewchum <piyachai.kaewchum@agoda.com>
2024-02-26 16:38:42 +0700

    add socket stream

--- src/app/(page)/(protected)/dashboard/[ip]/real-time/_section/RealTimeCarImage.tsx
+++ src/app/(page)/(protected)/dashboard/[ip]/real-time/_section/RealTimeCarImage.tsx
@@ -1,13 +1,37 @@
-import Image from "next/image";
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 
 import Card from "@/components/Card";
 
 const RealTimeCarImage = () => {
+  const [src, setSrc] = useState("");
+  useEffect(() => {
+    const socket = io("http://localhost:3001"); // Replace with your server URL
+    // Listen for incoming messages
+    socket.on("response", (message) => {
+      const file = message.message;
+      const base64Image = `data:image/png;base64,${file.replaceAll('"', "")}`;
+
+      setSrc(base64Image);
+    });
+
+    socket.on("joiner", (message) => {
+      setSrc(JSON.stringify(message.message));
+      console.log(JSON.stringify(message.message));
+    });
+
+    socket.emit("response", "dwd", (message) => {
+      console.log(message + "dwdwd");
+    });
+  }, []);
+
   return (
     <Card className="relative h-full w-full p-3">
       <div className="relative h-full w-full">
-        <Image src="/images/mock-car.jfif" alt="mock car" className="object-cover" fill />
+        <img src={src} alt="mock car" className="h-full w-full object-cover" />
       </div>
     </Card>
   );
